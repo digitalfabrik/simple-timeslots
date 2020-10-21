240da255007c2f45a6ff1e672abdc4542a1e7b85
Sven Seeberg <mail@sven-seeberg.de>
2020-10-21 22:32:29 +0200

    Bookings filter by event

--- src/webgui/admin.py
+++ src/webgui/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Event, TimeSlot, Booking
 # Register your models here.
 
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ('timeslot__event',)
+
 admin.site.register(Event)
 admin.site.register(TimeSlot)
-admin.site.register(Booking)
