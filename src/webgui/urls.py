c7db3e54ddddc4a286bb34b5965093bbc2e41921
Sven Seeberg <mail@sven-seeberg.de>
2020-11-16 17:29:12 +0100

    Fix view method calls

--- src/webgui/urls.py
+++ src/webgui/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:event_id>', views.event, name='event'),
-    path('cancel/confirm/<str:token>', views.cancel, name='cancel_confirm'),
+    path('cancel/confirm/<str:token>', views.cancel_confirm, name='cancel_confirm'),
     path('cancel/<str:token>', views.cancel, name='cancel'),
 ]
